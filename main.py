fa13fcfe97877dcf6bffb296bf9628b6c3e9786a
too-much-magic <dylancrombie@outlook.com>
2024-12-31 19:57:22 -0700

    Game finished! Added text for showing the winner's score and the loser's score.

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ import math
 class Pong:
     """Larger class to manage the game."""
 
+    """
+    TODO:
+    Helper functions for:
+    - collisions
+    - end game
+    """
+
     def __init__(self):
         pygame.init()
         self.clock = pygame.time.Clock()
@@ -23,6 +30,9 @@ class Pong:
         self.enemy_bumper = pygame.Rect(800 - self.PADDING * 2, screen_center[1] - 50, self.PADDING, 100)
         self.ball = pygame.Rect(screen_center[0] - 5, screen_center[1] - 5, 10, 10)
 
+        self.player_score = 0
+        self.enemy_score = 0
+
         self.ball_velocity = [5,5]
         if random.randint(0, 1) == 0:
             self.ball_velocity[0] *= -1
@@ -31,6 +41,7 @@ class Pong:
 
         self.BASE0 = (131, 148, 150)
         self.BASE01 = (88, 110, 117)
+        self.BASE03 = (0, 43, 54)
 
     def run_game(self):
         while True:
@@ -38,8 +49,7 @@ class Pong:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            BASE03 = (0, 43, 54)
-            self.screen.fill(BASE03)
+            self.screen.fill(self.BASE03)
 
             top_wall = (0, 0, 800, self.PADDING)
             pygame.draw.rect(self.screen, self.BASE0, top_wall)
@@ -72,16 +82,39 @@ class Pong:
 
             if self.PADDING < self.ball.x < self.PADDING * 2 and \
                     self.player_bumper.y < self.ball.y < self.player_bumper.y + 100:
-                self.ball_velocity[0] *= -1
+                self.ball_velocity[0] = 5
 
             if self.ball.x + self.PADDING > 800 - self.PADDING * 2 and \
                     self.enemy_bumper.y < self.ball.y < self.enemy_bumper.y + 100:
-                self.ball_velocity[0] *= -1
+                self.ball_velocity[0] = -5
+
+            if self.ball.x < 0:
+                self.enemy_score += 1
+                self.ball.x = self.screen_rect.centerx
+                self.ball.y = self.screen_rect.centery
+                self.ball_velocity = [0,0]
+
+                print("enemy scored")
+
+            if self.ball.x > 800 - self.PADDING:
+                self.player_score += 1
+                self.ball.x = self.screen_rect.centerx
+                self.ball.y = self.screen_rect.centery
+                self.ball_velocity = [0,0]
+                print("player scored")
 
             self.ball.x += self.ball_velocity[0]
             self.ball.y += self.ball_velocity[1]
 
+            f = pygame.font.SysFont('IBM Plex Mono', 50)
+            player_score_image = f.render(str(self.player_score), True, self.BASE0, self.BASE03)
+            self.screen.blit(player_score_image, [self.screen_rect.centerx - 100, 50])
+
+            enemy_score_image = f.render(str(self.enemy_score), True, self.BASE0, self.BASE03)
+            self.screen.blit(enemy_score_image, [self.screen_rect.centerx + 65, 50])
+
             pygame.draw.rect(self.screen, self.BASE0, self.ball)
+
 
             pygame.display.flip()
             self.clock.tick(60)
