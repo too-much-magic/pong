08c8909715a063cc436d34616e6d62fe8560a168
too-much-magic <dylancrombie@outlook.com>
2024-12-31 18:13:41 -0700

    Arena finished.

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ class Pong:
         screen_center = self.screen_rect.center
 
         self.player_bumper = pygame.Rect(20, screen_center[1] - 50, 15, 100)
+        self.enemy_bumper = pygame.Rect(780, screen_center[1] - 50, 15, 100)
         self.base0 = (131, 148, 150)
+        self.base01 = (88, 110, 117)
 
     def run_game(self):
         while True:
@@ -27,14 +29,30 @@ class Pong:
             base03 = (0, 43, 54)
             self.screen.fill(base03)
 
+            top_wall = (0, 0, 800, 15)
+            pygame.draw.rect(self.screen, self.base0, top_wall)
+
+            bottom_wall = (0, 585, 800, 15)
+            pygame.draw.rect(self.screen, self.base0, bottom_wall)
+
+            for n in range(1, 20):
+                divider = (self.screen_rect.center[0] - 15, 30 * n, 15, 15)
+                pygame.draw.rect(self.screen, self.base01, divider)
+
             pygame.draw.rect(self.screen, self.base0, self.player_bumper)
+            pygame.draw.rect(self.screen, self.base0, self.enemy_bumper)
 
             keys = pygame.key.get_pressed()
 
-            if keys[pygame.K_UP]:
+            if keys[pygame.K_w]:
                 self.player_bumper.y -= 5
-            if keys[pygame.K_DOWN]:
+            if keys[pygame.K_s]:
                 self.player_bumper.y += 5
+
+            if keys[pygame.K_UP]:
+                self.enemy_bumper.y -= 5
+            if keys[pygame.K_DOWN]:
+                self.enemy_bumper.y += 5
 
             pygame.display.flip()
             self.clock.tick(60)
